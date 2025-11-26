ee8f9af7175d6dafc50d0fd53ee275b0ae604303
tcheeric <erict875@tuta.io>
2025-11-26 18:26:22 +0000

    fix: Update ping command response from 'ok' to 'pong'

--- src/daemon/admin/commands/ping.ts
+++ src/daemon/admin/commands/ping.ts
@@ -2,5 +2,5 @@ import { NDKKind, NDKRpcRequest } from "@nostr-dev-kit/ndk";
 import AdminInterface from "../index.js";
 
 export default async function ping(admin: AdminInterface, req: NDKRpcRequest) {
-    return admin.rpc.sendResponse(req.id, req.pubkey, "ok", NDKKind.NostrConnect);
+    return admin.rpc.sendResponse(req.id, req.pubkey, "pong", NDKKind.NostrConnect);
 }
